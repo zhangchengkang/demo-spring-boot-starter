805181f3135f7932b6f706b090f3abacdf8a34db
zhangchengkang <952538291@qq.com>
2020-06-04 17:45:49 +0800

    项目搭建

---
+++ test-spring-boot-starter/src/test/java/com/ksf/spring/boot/starter/test/TestSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksf.spring.boot.starter.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
